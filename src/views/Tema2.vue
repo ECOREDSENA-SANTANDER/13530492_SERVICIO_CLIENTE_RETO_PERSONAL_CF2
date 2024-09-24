a7ff92d7bd784591c8b1d7e9b9b74ee41ec0c196
lizmanchego <karinamanchego0416@gmail.com>
2024-09-24 16:25:52 -0500

    [MAQ] ADD Inicio/ Sintesis/ Glosario/ RefBiblio/ Cred

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,175 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Principios básicos de atención al cliente
+    p.mb-5 La gestión de la atención al cliente integra una serie de actividades encaminadas a satisfacer las necesidades y requerimiento de los clientes, por lo que se hace necesario apoyarse en los siguientes principios: 
+    .fullview.p-4
+      .row.justify-content-center.align-items-center.px-sm-0.px-lg-5.mb-0 
+        .col-sm-12.col-lg-12.mb-0.bg05.p-sm-0.p-lg-5 
+          SlyderA(tipo="b")
+              .px-5.py-1
+                h4 Actitud positiva
+                .row.justify-content-center.align-items-center.mb-4
+                  .col-sm-12.col-lg-2.d-none.d-lg-block
+                    img.w-75(src='@/assets/curso/temas/tema2/img2.svg', alt='')
+                  .col-sm-12.col-lg-10.bg06.p-4
+                    p.mb-0 La actitud es la disposición, la conducta que asume un sujeto ante una situación. Ahora bien hablar de actitud positiva es referirse a una posición optimista, proactiva, de disfrute, gozo por las acciones que debe desarrollar o frente a eventos que debe abordar. Es fundamental que los colaboradores asuman una actitud afable, amable, comprensiva, alegre, cordial, respetuosa, que busque generar empatía para atender a los clientes, esto es, actitud positiva. Esta actitud positiva se convierte ya en un elemento fundamental para la atención de clientes con características y demandas muy particulares. 
+                p.mb-0 Esta actitud positiva requiere de pensamientos positivos. Pensar en positivo, es poder identificar las cosas buenas que pueden encontrarse en situaciones de adversidad, esto va a permitir ser optimistas en la vida y poder avanzar. 
+                p.mb-4 Los trabajadores que desarrollan su labor con actitud positiva, tienen mente abierta, generan sinergia, empatía, trabajan en equipo y reconocen en los clientes el valor que ellos representan. Es sabido que los clientes en cinco minutos reconocen la actitud negativa en el funcionario que los atiende. Este primer momento genera una huella imborrable.
+                p.mb-0 Al hablar de una actitud positiva, se hace referencia a posición de alegría, entusiasmo, agrado, frente a actividades, situaciones o eventos que se deben realizar o abordar y que no siempre pueden ser agradables. 
+           
+              .px-5.py-1
+                h4 Saber escuchar
+                .row.justify-content-center.align-items-center.mb-4
+                  .col-sm-12.col-lg-2.d-none.d-lg-block
+                    img.w-75(src='@/assets/curso/temas/tema2/img3.svg', alt='')
+                  .col-sm-12.col-lg-10.bg06.p-4
+                    p.mb-0 Los funcionarios que desarrollan escucha activa demuestran una gran afinidad hacia las necesidades y requerimientos del cliente. La atención brindada, durante toda la transacción, contribuye a la calidad del servicio y ayuda en caso de presentarse alguna falla, inconformidad o mal entendido después de haber entregado el bien o servicio. Se convierte entonces en la mejor estrategia ante quejas y reclamaciones, pues permite que el cliente se sienta atendido, tenido en cuenta y reconocido por la empresa. 
+                p.mb-0 Para generar una comunicación asertiva (abierta y clara), se requieren tres elementos: escucha atenta, habla adecuada, oportunos silencios. 
+                p.mb-4 Hay una máxima oriental que dice: “Nadie pone más en evidencia su torpeza y mala crianza, que el que empieza a hablar antes de que pueda enlazar las palabras y estar atento a lo que el interlocutor pronuncia”. 
+                p.mb-0 Este ejercicio implica voluntad, compresión, empatía y va de corazón a corazón. El escuchar exige atención a lo que dicen los demás, actitud cordial y disposición, estos dos ingredientes son la base del genuino diálogo. Por otro lado, Rogers (1961), dice: consideración positiva incondicional (un conditional positive regard), hace referencia a una atención entusiasta, auténtica y receptiva hacia el otro, en este caso el cliente; es decir aceptar al otro tal cuál es. Así mismo, De Mello (1989), escribe: “La escucha es la cosa más difícil de hacer. Para escuchar de verdad, las dos partes en el diálogo han de estar abiertas, sin prejuicios, en entera disposición de comprender”.
+              
+              .px-5.py-1
+                .row.justify-content-center.align-items-center.mb-5
+                  .col-sm-12.col-lg-8
+                    h4 Incondicionalidad
+                    p.fw-bold.mt-5 “El cliente es primero”. 
+                    p Como lo escribe Rogers (1968), #[b “se trata de un sentimiento positivo que se exterioriza sin reserva ni juicios.”]
+                    .bg06.p-4
+                      p.mb-0 El principio de la incondicionalidad, significa la aceptación total de ese otro, es decir con sus cualidades y defectos. No importa que acción realice siempre será bien recibido y atendido y se tratará de dar respuesta a sus necesidades y requerimientos. Es decir que la aceptación no está sujeta a un dar, un recibir, un actuar, un sentir y que se brinda en todo momento y lugar. 
+
+                  .col-sm-12.col-lg-4.d-none.d-lg-block
+                    img(src='@/assets/curso/temas/tema2/img4.png', alt='')
+              
+              .px-5.py-1
+                .row.justify-content-center.align-items-center.mb-5
+                  .col-sm-12.col-lg-8
+                    h4 Amabilidad
+                    .bg06.p-4.my-4
+                      p.mb-0 #[b Conocer el nombre de su cliente.] Los seres humanos se reconocen y hacen parte de un entorno social (se reconocen) cuando se les da un nombre. En la actualidad se impone una cultura de antivalores, es decir prima la satisfacción del Yo y se desconoce al otro. No hay que desconocer que las tecnologías han traspasado las fronteras físicas, uniendo al mundo, pero han generado una sensación de aislamiento, de abandono, resquebrajando el tejido familia y social. 
+                    p.mb-4 Por lo anterior se hace urgente ir al rescate de los valores, de las normas de cortesía, de urbanidad, del gesto amable, para que medien en las interrelaciones de los sujetos y se generen ambientes de sana y pacífica convivencia. La vida se construye de pequeños detalles que engrandecen el alma; el contacto visual, la palabra amable, la escucha, la empatía, la serenidad, la sencillez, la nobleza, son joyas que hace bello el compartir con otros. 
+                    p.mb-0.fw-bold Recuerde: trate a los demás como le gustaría que lo traten a usted.
+                  .col-sm-12.col-lg-4.d-none.d-lg-block
+                    img(src='@/assets/curso/temas/tema2/img5.png', alt='')
+
+              .px-5.py-1
+                .row.justify-content-center.align-items-center.mb-5
+                  .col-sm-12.col-lg-8
+                    h4 Orden en su lugar de trabajo
+                    p.mb-4.mt-4 El mantener un puesto de trabajo ordenado y limpio, trae beneficios tanto para la organización como para el trabajador que se ven reflejados en la respuesta rápida y oportuna ante una necesidad tanto del cliente como de la misma empresa; disminución de niveles de estrés para los colaboradores, mejorando así su productividad; disminución del riesgo de un posible accidente, representado en ganancias económicas para la entidad; aumento de la imagen corporativa, imagen agradable y acogedora en el recuerdo del cliente; control de la información, fácil seguimiento a la información que se genera en la empresa como resultado de la actividad económica que desarrolla. 
+                    .bg06.p-4
+                      p.mb-0 Una estrategia que se aplica en las organizaciones es la teoría japonesa de las 5 S, la cual menciona la Corporación Autónoma Regional de Santander (2004) y propone implementar: 
+
+                  .col-sm-12.col-lg-4
+                    .bg01.p-5
+                      ul.lista-ul--color.mb-0
+                        li.d-flex
+                          i.fas.fa-phone-alt
+                          span #[b Seiton:] organización, situar elementos necesarios. 
+                        li.d-flex
+                          i.fas.fa-phone-alt
+                          span #[b Seiso:] limpieza, suprimir suciedad. 
+                        li.d-flex
+                          i.fas.fa-phone-alt
+                          span #[b Seiketsu:] higiene y visualización, señalizar anomalías. 
+                        li.d-flex.mb-0
+                          i.fas.fa-phone-alt
+                          span #[b Shitsuke:] disciplina y compromiso, seguir mejorando.  
+
+    Separador
+    #t_2_1.titulo-segundo.color-acento-contenido
+      h2 2.1  Consejos prácticos para brindar a los clientes un servicio excelente
+    p.mb-5 Para brindar a los clientes un excelente servicio debe tenerse en cuenta las siguientes máximas: 
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-sm-12.col-lg-4.pr-0-cus.d-none.d-lg-block.p-5.bgimg2
+        div &nbsp;
+      .col-sm-12.col-lg-6.bg07.p-5.align-content-center
+        h4.bg01.py-1.px-2 Conecte su cerebro con el de su cliente 
+        p.mb-4 Piense como piensa el cliente o quedará por fuera de sus expectativas. Pregúntese: 
+        ul.lista-ul--color.mb-0
+          li.d-flex
+            i.fas.fa-phone-alt
+            span ¿Será que le soy simpático? 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span ¿Me escuchará? 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span ¿Tendrá en cuenta mis interrogantes o palabras? 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span ¿Hará algo al respecto? 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span ¿Me ofrecerá lo mejor? 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span ¿A todos los atenderá de esa forma? 
+    .row.justify-content-center.align-items-center.mb-5
+      .col-sm-12.col-lg-1.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema2/img7.svg', alt='')
+
+      .col-sm-12.col-lg-11.bg08.p-3
+        p.mb-0 Se debe entonces motivar al cliente para que tenga sentido de pertenencia con el servicio. Ese es su cliente. Lo único que busca es que le reconozcan, que le reciban con agrado, que le escuchen, que le sugieran alternativas para encontrar solución a su necesidad o deseo, que le haga sentir bienvenido; no dé por hecho que se convertirá en su cliente, eso dependerá del trato que reciba.
+    p.mb-5 Trate al cliente como a usted le agrada que lo atiendan. La empresa se mantiene viva gracias a sus clientes, por tanto, merecen una excelente atención a través de una completa información y una buena orientación. 
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-9
+        TabsA.color-acento-botones.mb-5
+          .tarjeta.bg09.p-5(titulo="Establezca comunicación")
+            h4.mb-5 Establezca comunicación con su cliente 
+            p.mb-0 Sea cortés, “La primera impresión es la más importante”. Se dice que una persona se forma once impresiones de otra en los primeros siete segundos de contacto. 
+          .tarjeta.bg09.p-5(titulo="Establezca contacto visual ")
+            h4.mb-5 Establezca contacto visual 
+            p.mb-0 Conéctese con el cliente, de tal forma que pueda percibir todas sus emociones, actitudes inflexiones de la voz, ritmos y tonos emocionales. Mire directamente a los ojos al cliente y transmita lozanía, entusiasmo y sinceridad. Dele las gracias por haber entrado o llamado o haberlo visitado.
+
+          .tarjeta.bg09.p-5(titulo="Detecte las necesidades ")
+            h4.mb-5 Detecte las necesidades 
+            p.mb-4 No se necesita un producto o un servicio. Lo que requiere es lo que producirá en el cliente ese producto o servicio, los beneficios finales, la forma en que servirán para hacerlo más feliz, saludable, rico, sabio o bello. 
+            p.mb-2 Para averiguar en qué le puede servir usted a otros: 
+            ol.lista-ol--cuadro.mb-0
+              li
+                .lista-ol--cuadro__vineta
+                  span 1
+                | Pregunte: ¿En qué puedo servirle? 
+              li
+                .lista-ol--cuadro__vineta
+                  span 2
+                | Averigüe por qué entró o le llamó. 
+              li.mb-0
+                .lista-ol--cuadro__vineta
+                  span 3
+                | Haga preguntas abiertas para poder comprender mejor lo que necesita. 
+
+
+          .tarjeta.bg09.p-5(titulo="Escuche al cliente")
+            h4.mb-5 Escuche al cliente
+            p.mb-0 Su cliente solo le pide toda su atención, por favor escúchelo y entiéndalo; estas dos actitudes son primordiales en la generación de empatía. Según el doctor Albert Mahrabian, profesor de la Universidad de California, en La gran guía del lenguaje no verbal de Baró (2012): “la eficacia para comunicarse la ha establecido en los siguientes porcentajes: 7% oral, 38% tono de voz y 55% no oral – lenguaje corporal”. 
+            p.mb-4 Se puede entonces afirmar que el cuerpo habla aun cuando se esté en silencio.
+            p.mb-3 ¿Cómo mejorar la capacidad para escuchar al cliente? 
+            ul.lista-ul--color.mb-4
+              li.d-flex
+                i.fas.fa-phone-alt
+                span Escuche las palabras de la otra persona. 
+              li.d-flex
+                i.fas.fa-phone-alt
+                span Escuche el tono de voz. 
+              li.d-flex
+                i.fas.fa-phone-alt
+                span “Escuche” el lenguaje corporal. 
+            p.mb-0 Es importante tener presente que oír no es lo mismo que escuchar, escuchar también es entender, es conectarse emocionalmente con el cliente.
+
+
+          .tarjeta.bg09.p-5(titulo="Ayude al cliente")
+            h4.mb-5 Ayude al cliente
+            p.mb-4 ¡Ayúdele a obtener lo que desea! 
+            p.mb-4 Los hombres de negocios y los profesionales, para poder ser competitivos hoy, deben dejar de dirigir su enfoque hacia un producto o servicio para hacerlo en cambio hacia las necesidades del cliente. Tenga presente que una persona no compra su producto por lo que es, sino por lo que le producirá. Enfóquese hacia las necesidades del cliente no hacia el producto. 
+            p.mb-0 Las compañías para competir están dirigiendo su enfoque hacia las necesidades del cliente. Los clientes compran los beneficios finales que disfrutará.
+ 
+      
+      .col-sm-12.col-lg-3.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema2/img8.png', alt='')
+
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,96 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 El cliente
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-5.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema3/img1.png', alt='')
+      .col-sm-12.col-lg-7
+        p.mb-0 El cliente es factor de éxito de una empresa; gracias a sus demandas, sus deseos y sentidas necesidades permanece viva la empresa. Se considera cliente a toda sujeto que demande atención ya sea por requerir productos o servicios en una organización, por tanto se pueden identificar clientes internos (funcionarios de la entidad y clientes externos (no pertenecientes a la organización). 
+        p.mb-4 Todos los esfuerzos se deben orientar a dar respuesta a todas las expectativas del cliente, quien es el epicentro de todas las actividades de la organización, por ello se debe conocer y comprender las necesidades y deseos de los clientes con el fin de plantear estrategias que permitan satisfacerlo. 
+        p.mb-0 Para dar respuesta a los requerimientos de los clientes de forma efectiva, se hace necesario identificar algunos tipos de clientes y reconocer sus características. 
+    Separador
+    #t_3_1.titulo-segundo.color-acento-contenido
+      h2 3.1  Tipología de clientes  
+    p.mb-5 Existen diferentes formas de clasificar a los clientes, pero básicamente determina la catalogación, las reacciones y comportamientos que se ponen de manifiesto en el momento en que se entabla una transacción comercial, entre estas están:
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-4.bg10.d-flex.justify-content-center.items-items-center.px-4
+        img(src='@/assets/curso/temas/tema3/img2.svg', alt='')
+
+      .col-sm-12.col-lg-6
+        .bg01.py-4.px-5.br-10
+          ul.lista-ul--color.mb-0
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente grosero:] agresivo. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente polémico:] todo lo discute. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente arrogante - sabelotodo:] cree tener toda la información. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente desconfiado:] suspicaz, incrédulo. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente impulsivo:] actúa sin pensar. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente dubitativo - indeciso:] se le dificulta tomar decisiones. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente meticuloso - Minucioso:] detalla y revisa todo. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente apresurado - con mucha prisa.]
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente reservado:] callado. 
+            li.d-flex
+              i.fas.fa-phone-alt
+              span #[b Cliente afable:] amigable, cordial. 
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-7
+        p.mb-0 Una última clasificación hace referencia a cliente sin y con discapacidad. Los clientes con discapacidad son personas con limitaciones para interactuar socialmente de forma efectiva. 
+        p.mb-4 Para brindar una buena atención a un cliente con discapacidad, se debe contar con un mínimo de conocimiento de cada una de ellas (auditiva, visual, sordo-ceguera, física, cognitiva), este conocimiento le hará cambiar la concepción del discapacitado como un sujeto que tan solo merece lástima, a una persona que merece respeto y atención. 
+        p.mb-0 Según la normatividad nacional e internacional, las empresas deben brindar las condiciones necesarias para que personas con alguna limitación, puedan recibir el servicio que requieran. Para brindar una adecuada atención las empresas deben invertir en adecuar la infraestructura y los equipos tecnológicos. Las organizaciones deben por tanto brindar una atención incluyente a sus clientes.
+
+      .col-sm-12.col-lg-5.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema3/img3.png', alt='')
+
+
+
+    Separador
+    #t_3_2.titulo-segundo.color-acento-contenido
+      h2 3.2  El ciclo del servicio
+    p.mb-5 Existen diferentes formas de clasificar a los clientes, pero básicamente determina la catalogación, las reacciones y comportamientos que se ponen de manifiesto en el momento en que se entabla una transacción comercial, entre estas están:  
+    .row.justify-content-center.items-items-center.mb-5
+      .col-sm-12.col-lg-4.bg11.d-flex.justify-content-center.items-items-center.px-4
+        img(src='@/assets/curso/temas/tema3/img4.svg', alt='')
+
+      .col-sm-12.col-lg-8
+        p.mb-3 El ciclo de servicio es el conjunto de actividades que el cliente se ve obligado a realizar para adquirir un producto o servicio.
+        p.mb-5 Un ciclo del servicio se puede definir como una secuencia completa de interacciones entre el cliente y el proveedor, son indispensables para la adquisición de un producto o servicio que va a satisfacer las necesidades planteadas por el mismo. 
+        .cajon.color-primario.p-4.mb-4
+          p.mb-0 Se inicia con la percepción del producto o servicio, la generación de expectativas que el cliente crea frente al producto o servicio al inicio de un proceso de negociación, y finaliza con la ejecución de la transacción y la aceptación del producto al recibirlo.
+
+    
+    
+    
+    
+    
+    
+    Separador
+    #t_3_3.titulo-segundo.color-acento-contenido
+      h2 3.3  ¿Cómo realizar el abordaje de los diferentes clientes de forma asertiva?
+    .row.justify-content-center.align-items-center.mb-5
+      .col-sm-12.col-lg-1
+        img(src='@/assets/curso/temas/tema3/img5.svg', alt='')
+
+      .col-sm-12.col-lg-11
+        p.mb-0 Hablar de asertividad es referirse a saber decir “no” sin agredir a otro, es ser sincero sin llegar a ofender, es expresar lo que se piensa sin llegar a sentirse menospreciado o ridiculizado, es escuchar la crítica sin sentirse anulado. Es creer en lo que sabe, sin sentirse dueño de la verdad, es reconocer que existen infinidad de cristales para ver el mundo. Es descubrir que el otro puede tener algo que decir y enseñar. 
+
 
 </template>
 
